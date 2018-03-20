41a176a6f3066134bb5035c3d5f38c1923ea15c8
Nicole Hilden <nicole.hilden@gmail.com>
2018-03-19 23:23:40 -0400

    True false and mc

--- MultipleChoice.java
+++ MultipleChoice.java
@@ -1,6 +1,29 @@
 package M2;
 
-public class MultipleChoice {
+import java.util.ArrayList;
 
+public class MultipleChoice extends Question{
+	
+	public ArrayList<String> options = new ArrayList<String>();
+	
+	public String questionPrompt = new  String();
+	
+	
+	public void multipleChoice(){	
+		
+	}
+
+	//Gets the options
+	public ArrayList<String> getOptions(){
+		return options;
+	}
+	
+	//Sets the options
+	public void setOption(String s){
+		options.add(s);
+	}
+	
+	
+	
 }
 //
--- TrueFalse.java
+++ TrueFalse.java
@@ -1,5 +1,17 @@
 package M2;
 
+import java.util.ArrayList;
+
 public class TrueFalse {
 
+	public ArrayList<String> options = new ArrayList<String>();
+	
+	public String questionPrompt = new  String();
+	
+	//Initializes the options to True and False
+	public void trueFalse(){	
+		options.add("True");
+		options.add("False");
+	}
+ 
 }
