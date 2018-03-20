b4ced494037aacbc10b54af162a307c47649a298
csquilla <csquilla@stevens.edu>
2018-03-20 15:52:43 -0400

    add functionality for questionaire

--- Question.java
+++ Question.java
@@ -1,5 +1,19 @@
 package M2;
 
+import java.util.ArrayList;
+
 public abstract class Question {
 
+	public String questionPrompt = new  String();
+	
+	//Gets the options
+	public String getPrompt(){
+		return this.questionPrompt;
+	}
+		
+	//Sets the options
+	public void setPrompt(String s){
+		this.questionPrompt = s;
+	}
+	
 }
--- Questionaire.java
+++ Questionaire.java
@@ -3,9 +3,59 @@ package M2;
 import java.util.ArrayList;
 
 public class Questionaire {
+	
 	boolean isATest;
 	ArrayList<Question> questionsList;
 	int id;
 	String name;
+	
+	public Questionaire(){
+		
+	}
+	
+	public void setIsATest(boolean isATest) {
+		this.isATest = isATest;
+	}
+	
+	public boolean getIsATest() {
+		return this.isATest;
+	}
+
+	public ArrayList<Question> getQuestionsList() {
+		return questionsList;
+	}
+
+	public void addQuestion(Question q) {
+		this.questionsList.add(q);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void tabulateSurvey() {
+		//todo
+	}
+	
+	public void gradeTest() {
+		//todo
+	}
+	
+	
+	
+	
+	
 }
 
