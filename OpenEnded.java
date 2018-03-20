0ed22581f32c4865c9506a8b3f1a4e48e2309613
Nicole Hilden <nicole.hilden@gmail.com>
2018-03-19 23:36:29 -0400

    Open Ended

--- OpenEnded.java
+++ OpenEnded.java
@@ -2,4 +2,17 @@ package M2;
 
 public class OpenEnded extends Question{
 
+	int charLimit;
+	
+	String questionPrompt = new String();
+	
+	//Initializes the Open Ended question and sets character limit
+	public OpenEnded(String s){
+		if(s.equals("long")){
+			charLimit = 2000;
+		}
+		if(s.equals("short")){
+			charLimit = 250;
+		}
+	}
 }
